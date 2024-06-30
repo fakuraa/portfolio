6537894ad45aac878cc2d322d773b7437135425d
Fadil <fadil@suitmedia.com>
2024-06-30 19:40:31 +0700

    Deploy Version 0.0.2+2

---
+++ assets/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:fakuraa_portfolio/firebase_options.dart';
+import 'package:fakuraa_portfolio/modules/home/home_view.dart';
+import 'package:fakuraa_portfolio/routes/page_routes.dart';
+import 'package:fakuraa_portfolio/themes/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+Future<void> main() async {
+  if (kDebugMode) {
+    debugPrint = (String? message, {int? wrapWidth}) =>
+        debugPrintSynchronously(message, wrapWidth: wrapWidth);
+  } else {
+    debugPrint = (String? message, {int? wrapWidth}) {};
+  }
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveSizer(builder: (context, orientation, deviceType) {
+      return GetMaterialApp(
+        scrollBehavior: const MaterialScrollBehavior().copyWith(
+          dragDevices: {PointerDeviceKind.mouse, PointerDeviceKind.touch},
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: HomeView.page,
+        getPages: PageRoutes.pages,
+        theme: AppTheme.buildThemeData(false),
+        builder: (BuildContext context, child) {
+          return MediaQuery(
+            data: MediaQuery.of(context)
+                .copyWith(textScaler: const TextScaler.linear(1.0)),
+            child: child ?? Container(),
+          );
+        },
+      );
+    });
+  }
+}
